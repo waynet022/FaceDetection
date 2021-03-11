773e716d1eeb2476a0625dcba7c468cdb62ac092
wayne1tam <waynevick7226@hotmail.com>
2021-03-11 02:48:16 -0500

    Add neighbors argument and adjusted functions

--- face_detect.py
+++ face_detect.py
@@ -5,6 +5,7 @@ def parser():
     parser = argparse.ArgumentParser(description='Face Detection')
     parser.add_argument("--image", type=str, help='Path to the image')
     parser.add_argument("--video", type=str, help='Path to the video')
+    parser.add_argument("--neighbors", type=int, help='minNeighbors factor')
 
     return parser.parse_args()
 
@@ -15,10 +16,10 @@ def convert_gray(image, show=False):
         cv.waitKey(0)
     return output
 
-def detect_face(image, gray_image, show=True):
+def detect_face(image, gray_image, neighbors=3, show=True):
     hc_file = 'haar_face.xml'
     haar_cascade = cv.CascadeClassifier(hc_file)
-    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=neighbors)
     if show:
         print('Number of faces detected: ', len(faces_rect))
         for (x,y,w,h) in faces_rect:
@@ -31,4 +32,4 @@ if __name__=='__main__':
     image_file = args.image
     img = cv.imread(image_file)
     gray = convert_gray(img)
-    detect_face(img, gray)
+    detect_face(img, gray, args.neighbors)
