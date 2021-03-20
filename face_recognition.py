1b127a4287b9e0d439f37502bdf56fd531eb2fef
wayne1tam <waynevick7226@hotmail.com>
2021-03-19 20:14:13 -0400

    Wrap file in function

--- face_recognition.py
+++ face_recognition.py
@@ -1,30 +1,33 @@
 import numpy as np
 import cv2 as cv
 
-people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+def run():
+    people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 
-haar_cascade = cv.CascadeClassifier('haar_face.xml')
+    haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-features=np.load('features.npy', allow_pickle=True)
-labels=np.load('labels.npy')
+    features=np.load('features.npy', allow_pickle=True)
+    labels=np.load('labels.npy')
 
-face_recognizer = cv.face.LBPHFaceRecognizer_create()
-face_recognizer.read('face_trained.yml')
+    face_recognizer = cv.face.LBPHFaceRecognizer_create()
+    face_recognizer.read('face_trained.yml')
 
-img = cv.imread('images/faces/val/ben_afflek/2.jpg')
+    img = cv.imread('images/faces/val/ben_afflek/2.jpg')
+    cv.imshow('Person', img)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Person', img)
+    faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
 
-faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x,y,w,h) in faces_rect:
+        faces_roi = gray[y:y+h, x:x+w]
 
-for (x,y,w,h) in faces_rect:
-    faces_roi = gray[y:y+h, x:x+w]
+        label, confidence = face_recognizer.predict(faces_roi)
+        print(f'Label = {people[label]} with a confidence of {confidence}')
+        cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+        cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
 
-    label, confidence = face_recognizer.predict(faces_roi)
-    print(f'Label = {people[label]} with a confidence of {confidence}')
-    cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
-    cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+    cv.imshow('Detected Image', img)
+    cv.waitKey(0)
 
-cv.imshow('Detected Image', img)
-cv.waitKey(0)
+if __name__=='__main__':
+    run()
